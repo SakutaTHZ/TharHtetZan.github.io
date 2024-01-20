cf28c9abc953b9ed4d862785fe46d1298a183264
Sakuta <110976118+SakutaTHZ@users.noreply.github.com>
2024-01-20 12:28:00 +0630

    nav box added

--- js/btnActions.js
+++ js/btnActions.js
@@ -1,8 +1,10 @@
 // Side Bar Action
 const menu = document.querySelector(".menu");
+const navLinkBox = document.querySelector(".navLinkBox");
 
-window.addEventListener("click", () => {
+menu.addEventListener("click", () => {
     console.log("Open Menu")
     // Add / Remove class
     menu && menu.classList.contains('navOpen')?menu.classList.remove('navOpen'):menu.classList.add('navOpen')
+    navLinkBox && navLinkBox.classList.contains('navLinkBoxOpen')?navLinkBox.classList.remove('navLinkBoxOpen'):navLinkBox.classList.add('navLinkBoxOpen')
 });
