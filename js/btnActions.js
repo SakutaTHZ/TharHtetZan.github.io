790acd8bcfd2c9e5105d544e8633519503deb416
Sakuta <110976118+SakutaTHZ@users.noreply.github.com>
2024-01-27 15:41:01 +0630

    hope it works now

--- js/btnActions.js
+++ js/btnActions.js
@@ -13,19 +13,20 @@ const navLinks = document.querySelectorAll('.navLinkBox > li')
 const currentNav = document.querySelector('.currentNav')
 navLinks.forEach(nav => {
     nav.addEventListener("click",()=>{
-        navLinkBox.classList.remove('navLinkBoxOpen')
         console.log(`${nav.innerText} Opened`)
-        navLinks.forEach(allnav => {
-            allnav.classList.remove('activeNav')
-        })
-        nav.classList.add('activeNav')
         nav.classList.add('navClicked')
         setTimeout(() => {
             menu.classList.remove('navOpen')
+            navLinkBox.classList.remove('navLinkBoxOpen')
         }, 2500);
         setTimeout(() => {
             nav.classList.remove('navClicked')
         }, 3000);
         currentNav.innerHTML=nav.dataset.name
+        
+        navLinks.forEach(allnav => {
+            allnav.classList.remove('activeNav')
+        })
+        nav.classList.add('activeNav')
     })
 });
