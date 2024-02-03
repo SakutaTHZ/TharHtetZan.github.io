0fa5cec91db395ad865aa14ee4032ba372ba3bc9
Sakuta <110976118+SakutaTHZ@users.noreply.github.com>
2024-02-03 23:03:26 +0630

    updated about me

--- js/btnActions.js
+++ js/btnActions.js
@@ -5,7 +5,12 @@ const navLinkBox = document.querySelector(".navLinkBox")
 const navs = document.querySelectorAll('.navLinkBox>li')
 
 menu.addEventListener("click", () => {
-    console.log("Open Menu")
+    // Add / Remove class
+    menu && menu.classList.contains('navOpen')?menu.classList.remove('navOpen'):menu.classList.add('navOpen')
+    navLinkBox && navLinkBox.classList.contains('navLinkBoxOpen')?navLinkBox.classList.remove('navLinkBoxOpen'):navLinkBox.classList.add('navLinkBoxOpen')
+})
+
+document.querySelector(".closeBox").addEventListener("click", () => {
     // Add / Remove class
     menu && menu.classList.contains('navOpen')?menu.classList.remove('navOpen'):menu.classList.add('navOpen')
     navLinkBox && navLinkBox.classList.contains('navLinkBoxOpen')?navLinkBox.classList.remove('navLinkBoxOpen'):navLinkBox.classList.add('navLinkBoxOpen')
@@ -22,4 +27,11 @@ const removeActive= (navs,()=>{
     navs.forEach(element => {
         element.classList.remove('activeNav')
     })
+})
+
+window.addEventListener("keyup", (e) => {
+    if(e.key == "Escape") {
+        menu && menu.classList.contains('navOpen')?menu.classList.remove('navOpen'):menu.classList.add('navOpen')
+        navLinkBox && navLinkBox.classList.contains('navLinkBoxOpen')?navLinkBox.classList.remove('navLinkBoxOpen'):navLinkBox.classList.add('navLinkBoxOpen')
+    }
 })
