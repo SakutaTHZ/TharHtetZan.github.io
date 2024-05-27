9b3b5342481ae9233f364967245a3a382b570a36
Sakuta <110976118+SakutaTHZ@users.noreply.github.com>
2024-05-27 12:27:47 +0630

    added firework cursor effect

--- js/version1.js
+++ js/version1.js
@@ -207,3 +207,25 @@ const setSizes = () => {
 };
 
 setSizes();
+
+//add mouse click effect 
+let body=document.querySelector('body')
+body.addEventListener("click", (event)=>{
+  let spark=document.createElement('div')
+  spark.classList.add('spark')
+  body.appendChild(spark)
+
+  spark.style.top = (event.clientY - body.offsetTop)+'px'
+  spark.style.left = (event.clientX - body.offsetLeft)+'px'
+  spark.style.filter = `hue-rotate(${(Math.random()*360)}deg)`
+
+  for (let i = 0; i <= 7; i++) {
+    let span = document.createElement('span')
+    span.style.transform = `rotate(${(i*45)}deg)`
+    spark.appendChild(span)
+  }
+
+  setTimeout(() => {
+    spark.remove()
+  }, 500);
+})
